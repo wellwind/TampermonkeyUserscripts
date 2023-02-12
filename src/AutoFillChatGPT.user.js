4760cc3d6c6a41e01365efc75cb0afed381eac09
Mike Huang <wellwind@mail2000.com.tw>
2023-02-12 16:59:33 +0800

    fix: remove prompt query string

--- src/AutoFillChatGPT.user.js
+++ src/AutoFillChatGPT.user.js
@@ -18,21 +18,23 @@
   // 解析參數
   const prompt = decodeURIComponent(searchParams.get("prompt"));
 
-  // 更新網址
-  const newUrl = `${window.location.origin}${
-    window.location.pathname
-  }?${searchParams.toString()}`;
-  window.history.replaceState({}, "", newUrl);
+  if (prompt) {
+    // 隔一秒再處理，避免畫面還沒準備好
+    setTimeout(() => {
+      const textarea = document.querySelector("textarea[data-id=root]");
+      const button = textarea.parentElement.querySelector(
+        "button.absolute.p-1.rounded-md.text-gray-500.bottom-1\\.5.right-1.md\\:bottom-2\\.5.md\\:right-2.hover\\:bg-gray-100.dark\\:hover\\:text-gray-400.dark\\:hover\\:bg-gray-900.disabled\\:hover\\:bg-transparent.dark\\:disabled\\:hover\\:bg-transparent"
+      );
+      textarea.value = prompt.replace(/\\n/g, "\n");
+      button.click();
 
-  setTimeout(() => {
-    const textarea = document.querySelector("textarea[data-id=root]");
-    const button = textarea.parentElement.querySelector(
-      "button.absolute.p-1.rounded-md.text-gray-500.bottom-1\\.5.right-1.md\\:bottom-2\\.5.md\\:right-2.hover\\:bg-gray-100.dark\\:hover\\:text-gray-400.dark\\:hover\\:bg-gray-900.disabled\\:hover\\:bg-transparent.dark\\:disabled\\:hover\\:bg-transparent"
-    );
-    textarea.value = prompt.replace(/\\n/g, "\n");
-    button.click();
-
-    // 移除 prompt 參數
-    searchParams.delete("prompt");
-  }, 1000);
+      // 移除 prompt 參數
+      searchParams.delete("prompt");
+      // 更新網址
+      const newUrl = `${window.location.origin}${
+        window.location.pathname
+      }?${searchParams.toString()}`;
+      window.history.replaceState({}, "", newUrl);
+    }, 1000);
+  }
 })();
