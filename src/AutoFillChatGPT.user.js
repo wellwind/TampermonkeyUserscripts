d3736cea1ae3fa3aa8df4097dea936571b5104ec
Mike Huang <wellwind@mail2000.com.tw>
2023-02-13 09:33:07 +0800

    feat: add autoSubmit

--- src/AutoFillChatGPT.user.js
+++ src/AutoFillChatGPT.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Auto fill to ChatGPT
 // @description  Auto fill prompt to ChatGPT and get instantly result
-// @version      0.5
+// @version      1.0.0
 // @source       https://github.com/wellwind/TampermonkeyUserscripts/raw/main/src/AutoFillChatGPT.user.js
 // @namespace    https://github.com/wellwind/TampermonkeyUserscripts/raw/main/src/AutoFillChatGPT.user.js
 // @website      https://fullstackladder.dev/
@@ -20,17 +20,21 @@
 
   // 解析參數
   const prompt = searchParams.get("prompt");
+  const autoSubmit = searchParams.get("autoSubmit");
 
   if (prompt) {
     // 隔一秒再處理，避免畫面還沒準備好
     setTimeout(() => {
       const textarea = document.querySelector("textarea[data-id=root]");
-      const button = textarea.parentElement.querySelector(
-        "button.absolute.p-1.rounded-md.text-gray-500.bottom-1\\.5.right-1.md\\:bottom-2\\.5.md\\:right-2.hover\\:bg-gray-100.dark\\:hover\\:text-gray-400.dark\\:hover\\:bg-gray-900.disabled\\:hover\\:bg-transparent.dark\\:disabled\\:hover\\:bg-transparent"
-      );
       textarea.value = decodeURIComponent(prompt).replace(/\\n/g, "\n");
-      textarea.dispatchEvent(new Event('input', { bubbles: true }));
-      //button.click();
+      textarea.dispatchEvent(new Event("input", { bubbles: true }));
+
+      if (autoSubmit) {
+        const button = textarea.parentElement.querySelector(
+          "button.absolute.p-1.rounded-md.text-gray-500.bottom-1\\.5.right-1.md\\:bottom-2\\.5.md\\:right-2.hover\\:bg-gray-100.dark\\:hover\\:text-gray-400.dark\\:hover\\:bg-gray-900.disabled\\:hover\\:bg-transparent.dark\\:disabled\\:hover\\:bg-transparent"
+        );
+        button.click();
+      }
 
       // 移除 prompt 參數
       searchParams.delete("prompt");
