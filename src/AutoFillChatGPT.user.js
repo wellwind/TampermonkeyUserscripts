dd6e397431c715578898f433a006a65bbb231819
Mike Huang <wellwind@mail2000.com.tw>
2023-02-13 10:46:58 +0800

    remove appended query string

--- src/AutoFillChatGPT.user.js
+++ src/AutoFillChatGPT.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Auto fill to ChatGPT
 // @description  Auto fill prompt to ChatGPT and get instantly result
-// @version      1.1.1
+// @version      1.1.2
 // @source       https://github.com/wellwind/TampermonkeyUserscripts/raw/main/src/AutoFillChatGPT.user.js
 // @namespace    https://github.com/wellwind/TampermonkeyUserscripts/raw/main/src/AutoFillChatGPT.user.js
 // @website      https://fullstackladder.dev/
@@ -25,10 +25,12 @@
   if (prompt) {
     // 隔一秒再處理，避免畫面還沒準備好
     setTimeout(() => {
+      // 填入 prompt
       const textarea = document.querySelector("textarea[data-id=root]");
       textarea.value = decodeURIComponent(prompt).replace(/\\n/g, "\n");
       textarea.dispatchEvent(new Event("input", { bubbles: true }));
 
+      // 自動送出
       if (autoSubmit) {
         const button = textarea.parentElement.querySelector(
           "button.absolute.p-1.rounded-md.text-gray-500.bottom-1\\.5.right-1.md\\:bottom-2\\.5.md\\:right-2.hover\\:bg-gray-100.dark\\:hover\\:text-gray-400.dark\\:hover\\:bg-gray-900.disabled\\:hover\\:bg-transparent.dark\\:disabled\\:hover\\:bg-transparent"
@@ -37,6 +39,7 @@
       }
 
       // 移除參數
+      searchParams.delete("prompt");
       searchParams.delete("autoSubmit");
 
       // 更新網址
