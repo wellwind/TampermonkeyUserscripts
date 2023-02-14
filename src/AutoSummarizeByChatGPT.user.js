e15c45930ba4cc2e845beef5a13f96e7ff0ca71f
Mike Huang <wellwind@mail2000.com.tw>
2023-02-14 23:16:54 +0800

    feat: 不帶參數，直接跳轉

--- src/AutoSummarizeByChatGPT.user.js
+++ src/AutoSummarizeByChatGPT.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Auto summarize article by ChatGPT
 // @description  Send article to ChatGPT and get summarization result
-// @version      0.0.1
+// @version      0.1.0
 // @source       https://github.com/wellwind/TampermonkeyUserscripts/raw/main/src/AutoSummarizeByChatGPT.user.js
 // @namespace    https://github.com/wellwind/TampermonkeyUserscripts/raw/main/src/AutoSummarizeByChatGPT.user.js
 // @website      https://fullstackladder.dev/
@@ -10,17 +10,70 @@
 // @match        http://*/*
 // @match        https://*/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=openai.com
-// @run-at       context-menu
+// @grant        GM.setValue
+// @grant        GM.getValue
+// @grant        GM.registerMenuCommand
 // ==/UserScript==
 
-(function () {
+const setAutoFill = async () => {
+  // 隔一秒再處理，避免畫面還沒準備好
+  setTimeout(async () => {
+    const prompt = await GM.getValue("prompt", "");
+    if (prompt) {
+      // 填入 prompt
+      const textarea = document.querySelector("textarea[data-id=root]");
+      textarea.value = prompt;
+      textarea.dispatchEvent(new Event("input", { bubbles: true }));
+
+      const button = textarea.parentElement.querySelector(
+        "button.absolute.p-1.rounded-md.text-gray-500.bottom-1\\.5.right-1.md\\:bottom-2\\.5.md\\:right-2.hover\\:bg-gray-100.dark\\:hover\\:text-gray-400.dark\\:hover\\:bg-gray-900.disabled\\:hover\\:bg-transparent.dark\\:disabled\\:hover\\:bg-transparent"
+      );
+      button.click();
+
+      // 清除暫存的 prompt
+      await GM.setValue("prompt", "");
+    }
+  }, 1000);
+};
+
+const registerContextMenu = async () => {
+  GM.registerMenuCommand(
+    "Summarize this article",
+    async () => {
+      const promptBase =
+        "請使用流暢的繁體中文分析以下文章，提供我簡短的文章總結，並盡可能用條列的方式表示";
+
+      const article = document.querySelector("article");
+
+      // 有 article 標籤才處理
+      if (article) {
+        // 複製一份 DOM
+        const clonedArticle = article.cloneNode(true);
+
+        // 節省空間，移除 `<pre>` 標籤，因為通常是程式碼
+        const preTags = clonedArticle.querySelectorAll("pre");
+        preTags.forEach((tag) => {
+          tag.remove();
+        });
+
+        // 設定 prompt 並打開 ChatGPT
+        const prompt = `${promptBase}\n\n${clonedArticle.innerText}`;
+        await GM.setValue("prompt", prompt);
+        window.open("https://chat.openai.com/chat", "_blank");
+      }
+    },
+    "c"
+  );
+};
+
+(async function () {
   "use strict";
-  const promptBase = '請使用流暢的繁體中文分析以下文章，提供我簡短的文章總結，並盡可能用條列的方式表示';
-  const article = document.querySelector("article");
-  if (article) {
-    const prompt = encodeURIComponent(`${promptBase}\n\n${article.innerText}`);
-    window.open(
-      `https://chat.openai.com/chat?prompt=${prompt}&autoSubmit=true`
-    );
+
+  if (location.hostname === "chat.openai.com") {
+    console.log("submit");
+    await setAutoFill();
+  } else {
+    console.log("register");
+    await registerContextMenu();
   }
 })();
