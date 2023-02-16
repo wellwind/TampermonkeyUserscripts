c1b0d8f86b672785509e64062e3d39aa1a84f6f2
Will 保哥 <doggy.huang@gmail.com>
2023-02-16 10:19:40 +0800

    Update AutoFillChatGPT.user.js

--- src/AutoFillChatGPT.user.js
+++ src/AutoFillChatGPT.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         Auto fill to ChatGPT
-// @description  Auto fill prompt to ChatGPT and get instantly result
-// @version      2.0.1
+// @name         ChatGPT: 自動填入提示文字並自動送出
+// @description  自動填入 ChatGPT 提示文字並可設定自動送出提問
+// @version      2.0.2
 // @source       https://github.com/wellwind/TampermonkeyUserscripts/raw/main/src/AutoFillChatGPT.user.js
 // @namespace    https://github.com/wellwind/TampermonkeyUserscripts/raw/main/src/AutoFillChatGPT.user.js
 // @website      https://fullstackladder.dev/
