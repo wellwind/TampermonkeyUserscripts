9b826997f65efa746f906fec7ee8fb22f0100c7d
Mike Huang <wellwind@mail2000.com.tw>
2023-02-21 10:23:18 +0800

    feat: add translate command

--- src/AutoFillChatGPT.user.js
+++ src/AutoFillChatGPT.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ChatGPT: 自動填入提示文字並自動送出
 // @description  自動填入 ChatGPT 提示文字並可設定自動送出提問
-// @version      2.3.0
+// @version      2.3.1
 // @source       https://github.com/wellwind/TampermonkeyUserscripts/raw/main/src/AutoFillChatGPT.user.js
 // @namespace    https://github.com/wellwind/TampermonkeyUserscripts/raw/main/src/AutoFillChatGPT.user.js
 // @website      https://fullstackladder.dev/
@@ -45,6 +45,14 @@ const registerContextMenuToAutoFill = () => {
   GM.registerMenuCommand("簡化上述內容", async () => {
     fillAndSubmitText("請用簡短的方式說明上述內容");
   });
+  // translate to TC
+  GM.registerMenuCommand("翻譯成繁中", async () => {
+    fillAndSubmitText("請將上述內容翻譯成流暢的繁體中文");
+  });
+  // translate to EN
+  GM.registerMenuCommand("翻譯成英文", async () => {
+    fillAndSubmitText("請將上述內容翻譯成流暢的英文");
+  });
 };
 
 const autoFillFromSegment = () => {
@@ -63,7 +71,7 @@ const autoFillFromSegment = () => {
     .replace(/\n{3,}/gs, "\n\n")
     .replace(/^\s+|\s+$/gs, "");
   const autoSubmit = params.get("autoSubmit");
-  
+
   // 沒有 prompt 就不用作任何事情
   if (!params) {
     return;
