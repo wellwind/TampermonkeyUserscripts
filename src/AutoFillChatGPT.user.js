26cd3fe6bcfd5ff776dad0d55abac396ae370bf4
Mike Huang <wellwind@mail2000.com.tw>
2023-02-22 12:43:04 +0800

    feat: add continue button

--- src/AutoFillChatGPT.user.js
+++ src/AutoFillChatGPT.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ChatGPT: 自動填入提示文字並自動送出
 // @description  自動填入 ChatGPT 提示文字並可設定自動送出提問
-// @version      3.0.1
+// @version      3.1.1
 // @source       https://github.com/wellwind/TampermonkeyUserscripts/raw/main/src/AutoFillChatGPT.user.js
 // @namespace    https://github.com/wellwind/TampermonkeyUserscripts/raw/main/src/AutoFillChatGPT.user.js
 // @website      https://fullstackladder.dev/
@@ -79,6 +79,8 @@ const fillAndSubmitText = (test) => {
 };
 
 const defaultManualSubmitText = [
+  // exemplify
+  { text: "繼續", value: "繼續" },
   // exemplify
   { text: "舉例說明", value: "請舉例說明" },
   // expand
