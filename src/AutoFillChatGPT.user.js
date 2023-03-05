a14162e229b8056cfdcac112fb7875cb7e2db802
Mike Huang <wellwind@mail2000.com.tw>
2023-03-05 23:07:13 +0800

    Update AutoFillChatGPT.user.js

--- src/AutoFillChatGPT.user.js
+++ src/AutoFillChatGPT.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ChatGPT: 自動填入提示文字並自動送出
 // @description  自動填入 ChatGPT 提示文字並可設定自動送出提問
-// @version      3.1.4
+// @version      3.1.5
 // @source       https://github.com/wellwind/TampermonkeyUserscripts/raw/main/src/AutoFillChatGPT.user.js
 // @namespace    https://github.com/wellwind/TampermonkeyUserscripts/raw/main/src/AutoFillChatGPT.user.js
 // @website      https://fullstackladder.dev/
@@ -89,6 +89,8 @@ const defaultManualSubmitText = [
   { text: "提供細節", value: "請提供更多細節說明" },
   // explain
   { text: "解釋清楚", value: "請用更清楚的方式解釋" },
+  // list explain
+  { text: "條列總結", value: "請以條列式的方式總結上述內容" },
   // rewrite
   { text: "重寫內容", value: "請重寫上述內容" },
   // short
